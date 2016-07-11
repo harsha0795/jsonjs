ea52a8b5736e420e99476872a7b86f4b0d029c50
harsha0795 <harsha0795@gmail.com>
2016-07-11 13:02:03 +0530

    Update json.js

--- json.js
+++ json.js
@@ -11,10 +11,12 @@ var delay=1000;
 I have used this in setTimeout()
 function semantics : setTimeout(function,delay);
 */
-var job=[]; //JOB Title Information
+var intjsresult=[];//Interest Javascript Result Array
+var emp_int=[];//Employee Interest Array
+var job=[];
 
 
-//Fetching Data from JOB JSON Data
+//Fetching Data from INTEREST JSON Data
  $.ajax({ /*
 	function : $.ajax
 	definition : Perform an asynchronous HTTP (Ajax) request.
@@ -22,7 +24,7 @@ var job=[]; //JOB Title Information
 	Return : jqXHR 
 */
  	type:'GET', //Http GET Request
-          url: "http://korefabrik.herokuapp.com/api/JOB", //Url of the json data
+          url: "http://korefabrik.herokuapp.com/api/INTEREST", //Url of the json data
           dataType: "jsonp",/*here "json" is deprecated for XMLHttpRequest cannot be loaded. Reason is we are takig data from an external source
 			Actual Error : No Access Control over Cross Origin. However jsonp supports this feature
           */
@@ -34,16 +36,52 @@ var job=[]; //JOB Title Information
 					This takes json obj as a parameter an will iterata though the loop and pushes to javascript array 
           		 */
           		 for (var i = 0; i < results.length; i++) {
-          		job.push([results[i].JOB_ID,results[i].JOB_TITLE])
+          		intjsresult.push([results[i].INTEREST_ID,results[i].INTEREST_NAME,results[i].CATEGORY_ID]);
 }
  
-          	console.log(job);//For development tests purpose.
+          	console.log(intjsresult);//For development tests purpose.
 }
 });
 
 
+
+// The same explanation holds
+//Fetching Data from EMP_INTEREST JSON Data
+
+$.ajax({
+ 	type:'GET',
+          url: "http://korefabrik.herokuapp.com/api/EMP_INTEREST",
+          dataType: "jsonp",
+          async: false,
+
+          success: function(results) {
+          	for (var i = 0; i < results.length; i++) {
+              emp_int.push([results[i].EMP_ID,results[i].INTEREST_ID,results[i].CAT_ID]);
+            }
+      console.log(emp_int);
+        
+          	
+}
+});
+
+//Fetching Data from JOB JSON Data
+$.ajax({
+ 	type:'GET',
+          url: "http://korefabrik.herokuapp.com/api/JOB",
+          dataType: "jsonp",
+          async: false,
+
+          success: function(results) {
+          	for (var i = 0; i < results.length; i++) {
+          		job.push([results[i].JOB_ID,results[i].JOB_TITLE])
+          	}
+          	for (var i = 0; i < job.length; i++) {
+          		//console.log(job[i]);
+          	}
+}
+});
 //Fetching Data from Employee JSON Data
-var emp=[]; //Employee Array
+var emp=[];
 
 $.when($.ajax({
   type:'GET',
@@ -65,7 +103,7 @@ for (var i = 0; i < results.length; i++) {
 });
 
 //Fetching Data from Roles JSON Data
-var roles=[]; //Employees and their roles
+var roles=[];
 $.ajax({
   type:'GET',
           url: "http://korefabrik.herokuapp.com/api/ROLES",
@@ -76,12 +114,16 @@ $.ajax({
             for (var i = 0; i < results.length; i++) {
               roles.push([results[i].EMP_ID,results[i].JOB_ID]);
             }
+            execbar()
+            execpie()
+            google.charts.setOnLoadCallback(drawChart);
+
             
 }
 });
-
+job1=[];
 bar=[];// Bar graph data for Job roles
-//Pie chart for the Interests
+pie=[];//Pie chart for the Interests
 
 function execbar() {
 //This function returns the 2-D Array which contains the Different Job roles with no. of people in each job role 
@@ -101,6 +143,25 @@ bar.push([job[i][1],count]);
 }
 return bar;
 }
+function execpie() {
+	//This function returns the 2-D Array which contains the Different Interest with no. of people in each interest
+	var pie=[];
+
+pie.push(["Interest","No of Employees"]);
+
+for (var j = 0; j < intjsresult.length; j++) {
+	count=0
+for (var i = 0; i < emp_int.length; i++) {
+if(emp_int[i][1]==intjsresult[j][0])
+	count++;
+}
+if(count!=0)
+pie.push([intjsresult[j][1],count])
+}
+return pie;
+
+}
+
 
 
 /* Efficiency Information
